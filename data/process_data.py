9ebf04e51fedeef35c79c2ba52be5c77e7070c01
ajayrao1983 <36269573+ajayrao1983@users.noreply.github.com>
2021-10-09 14:17:18 -0400

    Added 'replace' option with 'if_exists' in the to_sql method. Also converts each category column into binary instead of integer

--- data/process_data.py
+++ data/process_data.py
@@ -1,8 +1,24 @@
+# Import Libraries
 import sys
 import pandas as pd
+import numpy as np
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    '''
+    This function reads csv files and merges them
+    
+    Parameters
+    ----------
+    messages_filepath : Path to csv file containing messages
+    categories_filepath : Path to csv file containing categories
+
+
+    Returns
+    -------
+    df : Merged data frame
+
+    '''
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = messages.merge(categories, on = 'id')
@@ -10,6 +26,21 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+    '''
+    This function cleans the data by splitting categories into individual
+    columns, and retaining only the score as integer.
+    It also converts each column into binary.
+    
+    Parameters
+    ----------
+    df : Merged dataframe to be cleaned
+
+
+    Returns
+    -------
+    df : Cleaned dataframe
+
+    '''
     categories = df.categories.str.split(pat = ";", expand = True)
     row = categories.iloc[0]
     category_colnames = list(row.str.split(pat = "-", expand = True)[0])
@@ -22,6 +53,9 @@ def clean_data(df):
         # convert column from string to numeric
         categories[column] = categories[column].astype(int)
         
+        # convert values greater than 0 to 1
+        categories[column] = np.where(categories[column] > 0, 1, 0)
+        
     df.drop(['categories'], axis = 1, inplace = True)
     df = pd.concat([df, categories], axis = 1)
     
@@ -31,8 +65,22 @@ def clean_data(df):
     
 
 def save_data(df, database_filename):
+    '''
+    This function saves the dataframe in sqllite database
+    
+    Parameters
+    ----------
+    df : Cleaned dataframe to be saved
+    database_filename: Path of database where the dataframe is to be saved
+
+
+    Returns
+    -------
+    None
+
+    '''
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('DRResponse_Ajay', engine, index=False) 
+    df.to_sql('DRResponse_Ajay', engine, index=False, if_exists = 'replace') 
 
 
 def main():
